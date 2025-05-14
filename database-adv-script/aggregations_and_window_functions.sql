c1fa24b6c656be2765242019bc5471e047ae1e6b
Otieno Onyango Kelvin <kelvinonyango155@gmail.com>
2025-05-14 10:08:45 -0700

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,6 +1,10 @@
-SELECT user_id, COUNT(*) as total_bookings
+SELECT 
+    user_id, 
+    COUNT(*) as total_bookings
 FROM bookings
-GROUP BY user_id;
+GROUP BY user_id
+ORDER BY total_bookings DESC;
+
 
 SELECT 
     p.property_id,
@@ -9,4 +13,17 @@ SELECT
     RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) as booking_rank
 FROM properties p
 LEFT JOIN bookings b ON p.property_id = b.property_id
-GROUP BY p.property_id, p.property_name;
+GROUP BY p.property_id, p.property_name
+ORDER BY booking_rank;
+
+
+SELECT 
+    p.property_id,
+    p.property_name,
+    COUNT(b.booking_id) as total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) as row_num
+FROM properties p
+LEFT JOIN bookings b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.property_name
+ORDER BY total_bookings DESC;
+
