c1c002a7173a84dcf13e32e67e3253f314109e04
Otieno Onyango Kelvin <kelvinonyango155@gmail.com>
2025-05-14 09:42:35 -0700

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,8 +4,8 @@ INNER JOIN users u ON b.user_id = u.user_id;
 
 SELECT p.*, r.*
 FROM properties p
-ORDER BY property_id
 LEFT JOIN reviews r ON p.property_id = r.property_id;
+ORDER BY p.property_id
 
 SELECT u.*, b.*
 FROM users u
